493d7c7a07d7500e90943014174ebc144c06c2a6
miya0001 <miya@wpist.me>
2016-11-13 12:20:09 +0900

    add some utility functions and tests

--- lib/functions.php
+++ lib/functions.php
@@ -4,6 +4,11 @@ use WP_CLI\Utils;
 
 class Scaffold_VCCW
 {
+	/**
+	 * Get URL of the latest VCCW.
+	 *
+	 * @return string Return URL or WP_Error object.
+	 */
 	public static function get_latest_vccw_url()
 	{
 		$api = "https://api.github.com/repos/vccw-team/vccw/releases/latest";
@@ -17,5 +22,134 @@ class Scaffold_VCCW
 			$body = json_decode( $res['body'] );
 			return $body->zipball_url;
 		}
+	}
+
+	/**
+	 * Unzip
+	 *
+	 * @param string $src  Path to the .zip archive.
+	 * @param string $dest Path to extract .zip.
+	 * @return string      `true` or WP_Error object.
+	 */
+	public static function unzip( $src, $dest )
+	{
+		if ( ! is_file( $src ) ) {
+			return new WP_Error( "No such file or directory." );
+		}
+		$zip = new ZipArchive;
+		$res = $zip->open( $src );
+		if ( true === $res ) {
+			// extract it to the path we determined above
+			$zip->extractTo( $dest );
+			$zip->close();
+			return true;
+		}
+		return new WP_Error( "Can not open {$src}." );
+	}
+
+	/**
+	 * Create a temporary working directory
+	 *
+	 * @param  string $prefix Prefix for the temporary directory you want to create.
+	 * @return string         Path to the temporary directory.
+	 */
+	public static function tempdir( $prefix = '' )
+	{
+		$tempfile = tempnam( sys_get_temp_dir(), $prefix );
+		if ( file_exists( $tempfile ) ) {
+			unlink( $tempfile );
+		}
+		mkdir( $tempfile );
+		if ( is_dir( $tempfile ) ) {
+			return $tempfile;
+		}
+	}
+
+	/**
+	 * Copy directory recursively.
+	 *
+	 * @param  string $source  Path to the source directory.
+	 * @param  string $dest    Path to the destination.
+	 * @param  array  $exclude An array of the files to exclude.
+	 * @return void
+	 */
+	public static function rcopy( $src, $dest, $exclude = array() )
+	{
+		$src = untrailingslashit( $src );
+		$dest = untrailingslashit( $dest );
+		if ( ! is_dir( $dest ) ) {
+			mkdir( $dest, 0755 );
+		}
+		$iterator = self::get_files( $src );
+		foreach ( $iterator as $item ) {
+			if ( $item->isDir() ) {
+				if ( ! is_dir( $dest . '/' . $iterator->getSubPathName() ) ) {
+					mkdir( $dest . '/' . $iterator->getSubPathName() );
+				}
+			} else {
+				if ( ! in_array( $iterator->getSubPathName(), $exclude ) ) {
+					copy( $item, $dest . '/' . $iterator->getSubPathName() );
+				}
+			}
+		}
+	}
+
+	/**
+	 * Remove a directory recursively.
+	 *
+	 * @param  string $dir Path to the directory you want to remove.
+	 * @return void
+	 */
+	public static function rrmdir( $dir )
+	{
+		self::rempty( $dir );
+		rmdir( $dir );
+	}
+
+	/**
+	 * Empty a directory recursively.
+	 *
+	 * @param  string $dir     Path to the directory you want to remove.
+	 * @param  array  $exclude An array of the files to exclude.
+	 * @return void
+	 */
+	public static function rempty( $dir, $excludes = array() )
+	{
+		$dir = untrailingslashit( $dir );
+		$files = self::get_files( $dir, RecursiveIteratorIterator::CHILD_FIRST );
+		foreach ( $files as $fileinfo ) {
+			if ( $fileinfo->isDir() ) {
+				$skip = false;
+				foreach ( $excludes as $exclude ) {
+					if ( 0 === strpos( $exclude, $files->getSubPathName() ) ) {
+						$skip = true;
+					}
+				}
+				if ( ! $skip ) {
+					rmdir( $fileinfo->getRealPath() );
+				}
+			} else {
+				if ( ! in_array( $files->getSubPathName(), $excludes ) ) {
+					unlink( $fileinfo->getRealPath() );
+				}
+			}
+		}
+	}
+
+	/**
+	 * Get file's iterator object from the directory.
+	 *
+	 * @param string $dir   Path to the directory.
+	 * @param string $flags Flags for the `RecursiveIteratorIterator()`.
+	 * @return string       Literator object of the `RecursiveIteratorIterator()`.
+	 */
+	public static function get_files( $dir, $flags = RecursiveIteratorIterator::SELF_FIRST )
+	{
+		$dir = untrailingslashit( $dir );
+		$iterator = new RecursiveIteratorIterator(
+			new RecursiveDirectoryIterator( $dir, RecursiveDirectoryIterator::SKIP_DOTS ),
+			$flags
+		);
+		return $iterator;
 	}
 }
--- tests/test-functions.php
+++ tests/test-functions.php
@@ -4,10 +4,125 @@ class Scaffold_VCCW_Test extends WP_UnitTestCase
 {
 	/**
 	 * @test
+	 * @since 0.1.0
 	 */
 	public function get_latest_vccw_url()
 	{
 		$url = Scaffold_VCCW::get_latest_vccw_url();
 		$this->assertFalse( is_wp_error( $url ) );
+	}
+
+	/**
+	 * Tests for the `Scaffold_VCCW::rempty()`.
+	 *
+	 * @test
+	 * @since 0.1.0
+	 */
+	public function rempty()
+	{
+		$dir = self::mockdir();
+		$files = Scaffold_VCCW::get_files( $dir );
+		$this->assertSame( 7, iterator_count($files) );
+		$dir = self::mockdir();
+		Scaffold_VCCW::rempty( $dir );
+		$files = Scaffold_VCCW::get_files( $dir );
+		$this->assertSame( 0, iterator_count($files) );
+		$dir = self::mockdir();
+		Scaffold_VCCW::rempty( $dir, array(
+			"dir02/dir02-01.txt",
+			"dir01/dir01-01/dir01-01-01.txt"
+		) );
+		$files = Scaffold_VCCW::get_files( $dir );
+		$this->assertSame( 5, iterator_count($files) );
+	}
+
+	/**
+	 * Tests for the `Scaffold_VCCW::tempdir()`.
+	 *
+	 * @test
+	 * @since 0.1.0
+	 */
+	public function tempdir()
+	{
+		$dir = Scaffold_VCCW::tempdir();
+		$this->assertTrue( is_dir( $dir ) ); // $dir should exists.
+	}
+
+	/**
+	 * Tests for the `Scaffold_VCCW::rrmdir()`.
+	 *
+	 * @test
+	 * @since 0.1.0
+	 */
+	public function rrmdir()
+	{
+		$dir = self::mockdir();
+		$this->assertTrue( is_dir( $dir ) ); // $dir should exists.
+		Scaffold_VCCW::rrmdir( $dir );
+		$this->assertFalse( is_dir( $dir ) ); // $dir should not exists.
+	}
+
+	/**
+	 * Tests for the `Scaffold_VCCW::rcopy()`.
+	 *
+	 * @test
+	 * @since 0.1.0
+	 */
+	public function rcopy()
+	{
+		$src = self::mockdir();
+		$this->assertTrue( is_dir( $src ) ); // $dir should exists.
+		$dest = Scaffold_VCCW::tempdir();
+		$this->assertTrue( is_dir( $dest ) ); // $dir should exists.
+		$this->assertTrue( self::md5sum( $src ) !== self::md5sum( $dest ) );
+		// Copy directory recursively then check md5.
+		Scaffold_VCCW::rcopy( $src, $dest );
+		$this->assertTrue( self::md5sum( $src ) === self::md5sum( $dest ) );
+		$dest = Scaffold_VCCW::tempdir();
+		Scaffold_VCCW::rcopy( $src, $dest, array( "dir01/dir01-01.txt" ) );
+		$this->assertFalse( is_file( $dest . '/dir01/dir01-01.txt' ) );
+		$this->assertTrue( is_file( $dest . '/dir01/dir01-02.txt' ) );
+		$this->assertTrue( is_file( $dest . '/dir02/dir02-01.txt' ) );
+	}
+
+	/**
+	 * Create files and directories as mock for the test.
+	 *
+	 * @since  0.1.0
+	 * @return string $dir Path to the temporary directory.
+	 */
+	private static function mockdir()
+	{
+		$dir = Scaffold_VCCW::tempdir();
+		mkdir( $dir . "/dir01" );
+		file_put_contents( $dir . "/dir01/dir01-01.txt", time() );
+		file_put_contents( $dir . "/dir01/dir01-02.txt", time() );
+		mkdir( $dir . "/dir01/dir01-01" );
+		file_put_contents( $dir . "/dir01/dir01-01/dir01-01-01.txt", time() );
+		mkdir( $dir . "/dir02" );
+		file_put_contents( $dir . "/dir02/dir02-01.txt", time() );
+		return $dir;
+	}
+
+	/**
+	 * Create a md5 hash from directory.
+	 *
+	 * @since  0.1.0
+	 * @param  strint $dir Path to the directory.
+	 * @return string      Hash of the files in the derectory.
+	 */
+	private static function md5sum( $dir )
+	{
+		if ( ! is_dir( $dir ) ) {
+			return false;
+		}
+		$iterator = Scaffold_VCCW::get_files( $dir );
+		$md5 = array();
+		foreach ( $iterator as $item ) {
+			if ( ! $item->isDir() ) {
+				$md5[] = md5_file( $item );
+			}
+		}
+		return md5( implode( '', $md5 ) );
 	}
 }
